fbaa0e883b38ab32b11ff498793c94c6b16b3649
soulwax <konstantin.kling@braincon.de>
2022-05-27 02:35:24 +0000

    adding Prettier formatting key to package-json and removing semicolons

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 // All solutions below work with discord.js@13.7.0
 
 //#region ENVIRONMENT VARIABLES
-require(`dotenv`).config();
-const SHODAN_TOKEN = process.env.SHODAN_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
+require(`dotenv`).config()
+const SHODAN_TOKEN = process.env.SHODAN_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
 //#endregion
 
 //#region REQUIRES
-const { MessageEmbed } = require(`discord.js`);
-const { SlashCommandBuilder } = require(`@discordjs/builders`);
-const { REST } = require(`@discordjs/rest`);
-const { Routes } = require(`discord-api-types/v9`);
-const { Client } = require(`discord.js`);
+const { MessageEmbed } = require(`discord.js`)
+const { SlashCommandBuilder } = require(`@discordjs/builders`)
+const { REST } = require(`@discordjs/rest`)
+const { Routes } = require(`discord-api-types/v9`)
+const { Client } = require(`discord.js`)
 //#endregion
 
 //#region REST + CLIENT API + INTENTS
-const rest = new REST({ version: `9` }).setToken(SHODAN_TOKEN);
+const rest = new REST({ version: `9` }).setToken(SHODAN_TOKEN)
 const client = new Client({
   intents: [
     ,
@@ -36,117 +35,117 @@ const client = new Client({
     `DIRECT_MESSAGE_TYPING`,
     `GUILD_SCHEDULED_EVENTS`,
   ],
-});
+})
 //#endregion
 
 //#region COMMANDS
 const commands = [
   new SlashCommandBuilder()
-    .setName("ping")
+    .setName('ping')
     .setDescription('Replies with "pong" if the bot is online.'),
   new SlashCommandBuilder()
-    .setName("members")
-    .setDescription("Lists members of a given server."),
+    .setName('members')
+    .setDescription('Lists members of a given server.'),
   new SlashCommandBuilder()
-    .setName("avatar")
-    .setDescription("Embed avatar of a user (prototype).")
+    .setName('avatar')
+    .setDescription('Embed avatar of a user (prototype).')
     .addMentionableOption((option) =>
       option
-        .setName("user")
-        .setDescription("The user whose avatar you want to get.")
+        .setName('user')
+        .setDescription('The user whose avatar you want to get.')
         .setRequired(true)
     ),
   new SlashCommandBuilder()
-    .setName("example")
-    .setDescription("Replies with an example embed."),
+    .setName('example')
+    .setDescription('Replies with an example embed.'),
   new SlashCommandBuilder()
-    .setName("echo")
-    .setDescription("Echos a message.")
+    .setName('echo')
+    .setDescription('Echos a message.')
     .addStringOption((option) =>
       option
-        .setName("message")
-        .setDescription("The message you want to echo.")
+        .setName('message')
+        .setDescription('The message you want to echo.')
         .setRequired(true)
     ),
   new SlashCommandBuilder()
-    .setName("help")
-    .setDescription("Lists all commands."),
-];
+    .setName('help')
+    .setDescription('Lists all commands.'),
+]
 //#endregion
 
 //#region REFRESH
-(async () => {
+;(async () => {
   try {
-    console.log(`Started refreshing application (/) commands.`);
+    console.log(`Started refreshing application (/) commands.`)
 
     await rest.put(Routes.applicationCommands(CLIENT_ID), {
       body: commands,
-    });
-    
-    console.log(`Successfully reloaded application (/) commands.`);
+    })
+
+    console.log(`Successfully reloaded application (/) commands.`)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
 //#endregion
 
 client.on(`ready`, () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
 client.on(`interactionCreate`, async (interaction) => {
-  if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return
 
   if (interaction.commandName === `ping`) {
     // Measure response time ping
-    const ping = Date.now() - interaction.createdTimestamp.valueOf();
+    const ping = Date.now() - interaction.createdTimestamp.valueOf()
     const embed = new MessageEmbed()
       .setTitle(`Pong!`)
       .setDescription(`${ping}ms`)
-      .setColor(`#00ff00`);
-    await interaction.reply({ embeds: [embed] });
+      .setColor(`#00ff00`)
+    await interaction.reply({ embeds: [embed] })
   }
 
   if (interaction.commandName === `echo`) {
     const embed = new MessageEmbed()
       .setTitle(`Echo`)
       .setDescription(interaction.options.getString(`message`))
-      .setColor(`#00ff00`);
-    await interaction.reply({ embeds: [embed] });
+      .setColor(`#00ff00`)
+    await interaction.reply({ embeds: [embed] })
   }
 
   if (interaction.commandName === `members`) {
-    const guild = interaction.guild;
-    const members = await guild.members.fetch();
-    const memberList = members.map((member) => member.user.tag).join(`\n`);
+    const guild = interaction.guild
+    const members = await guild.members.fetch()
+    const memberList = members.map((member) => member.user.tag).join(`\n`)
     const embed = new MessageEmbed()
       .setTitle(`${guild.name}'s Members`)
       .setDescription(memberList)
-      .setColor(`#0099ff`);
-    await interaction.reply({ embeds: [embed] });
+      .setColor(`#0099ff`)
+    await interaction.reply({ embeds: [embed] })
   }
 
   if (interaction.commandName === `avatar`) {
     // Get user from interaction or the first argument
-    const user = interaction.options.getMentionable(`user`);
+    const user = interaction.options.getMentionable(`user`)
 
-    const userName = user.user.tag;
-    const userAvatar = user.displayAvatarURL();
+    const userName = user.user.tag
+    const userAvatar = user.displayAvatarURL()
 
     if (!user || !userName || !userAvatar) {
       //send error embed message
       const embed = new MessageEmbed()
         .setTitle(`Error`)
         .setDescription(`Please mention a user.`)
-        .setColor(`#ff0000`);
-      await interaction.reply({ embeds: [embed] });
+        .setColor(`#ff0000`)
+      await interaction.reply({ embeds: [embed] })
     } else {
       const embed = new MessageEmbed()
         .setTitle(`${userName}'s Avatar`)
         .setDescription(`[Link to avatar](${userAvatar})`)
         .setImage(userAvatar)
-        .setColor(`#0099ff`);
-      await interaction.reply({ embeds: [embed] });
+        .setColor(`#0099ff`)
+      await interaction.reply({ embeds: [embed] })
     }
   }
 
@@ -175,12 +174,12 @@ client.on(`interactionCreate`, async (interaction) => {
       .setFooter({
         text: `Some footer text here`,
         iconURL: `https://i.imgur.com/AfFp7pu.png`,
-      });
-    if (exampleEmbed === `undefined`) return;
-    await interaction.reply({ embeds: [exampleEmbed] });
+      })
+    if (exampleEmbed === `undefined`) return
+    await interaction.reply({ embeds: [exampleEmbed] })
   }
 
-  if(interaction.commandName === `help`) {
+  if (interaction.commandName === `help`) {
     const embed = new MessageEmbed()
       .setTitle(`Help`)
       .setDescription(`List of all commands:`)
@@ -191,10 +190,10 @@ client.on(`interactionCreate`, async (interaction) => {
         url: `${client.user.displayAvatarURL()}`,
       })
     for (const command of commands) {
-      embed.addField(command.name, command.description);
+      embed.addField(command.name, command.description)
     }
-    await interaction.reply({ embeds: [embed] });
+    await interaction.reply({ embeds: [embed] })
   }
-});
+})
 
-client.login(SHODAN_TOKEN);
+client.login(SHODAN_TOKEN)
