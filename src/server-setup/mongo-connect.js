bfedf950bf07ad2ddc6a585f48172d2b78bf6d13
soulwax <soulwax-95@protonmail.com>
2023-07-03 03:46:45 +0000

    ...part 2

---
+++ src/server-setup/mongo-connect.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const chalk = require('chalk')
+
+const bold = chalk.bold
+
+const { cyan, yellow, red, green } = bold
+
+const connected = green
+const error = yellow
+const disconnected = red
+const connectionDetails = cyan
+const dbUrl = process.env.MONGODB_CONNECTION_STRING
+
+// export let db
+// export this function and imported by server.js
+
+const mongoConnect = async () => {
+  mongoose.set('strictQuery', true)
+  mongoose.connect(dbUrl)
+  const db = mongoose.connection
+  db.on('connected', () => {
+    // save the DB_URI as string from .env file, cut out username and password, and replace with ****
+    const dbUrlHidden = dbUrl.replace(/\/\/(.*):(.*)@/, '//****:****@')
+
+    console.log(connected(`Mongoose default connection is open to ${connectionDetails(dbUrlHidden)}`))
+  })
+
+  db.on('error', (err) => {
+    console.log(error(`Mongoose default connection has occured ${err} error`))
+  })
+
+  db.on('disconnected', () => {
+    console.log(disconnected('Mongoose default connection is disconnected'))
+  })
+}
+
+module.exports = mongoConnect
